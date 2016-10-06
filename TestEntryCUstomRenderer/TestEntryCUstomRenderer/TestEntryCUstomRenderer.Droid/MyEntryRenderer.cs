486b42b16302790e340e5e5117d82ea7d0f241fa
kkb <kkbpower@naver.com>
2016-10-06 15:49:33 +0900

    entry custom renderer 추가

---
+++ TestEntryCUstomRenderer/TestEntryCUstomRenderer/TestEntryCUstomRenderer.Droid/MyEntryRenderer.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using TestEntryCUstomRenderer;
+using TestEntryCUstomRenderer.Droid;
+
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace TestEntryCUstomRenderer.Droid
+{
+    class MyEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(global::Android.Graphics.Color.LightGreen);
+            }
+        }
+    }
+}
